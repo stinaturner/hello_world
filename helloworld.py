31b1b4e97a083df58aa86fb2b05ecbb741d075b3
Michael T. Mordowanec <michael.mordowanec@gmail.com>
2016-02-20 17:20:13 -0500

    formatting

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,17 @@
-x = 'hello world'
+#!/usr/bin/python
+
+
 def fivetimes(string):
     for i in range(5):
         print string
-fivetimes(7)
-fivetimes(x)
-y = 'butt'
-fivetimes(y)
+
+
+def main():
+    x = 'hello world'
+    fivetimes(7)
+    fivetimes(x)
+    y = 'butt'
+    fivetimes(y)
+
+if __name__ == '__main__':
+    main()
